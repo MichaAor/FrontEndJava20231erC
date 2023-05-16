2ed5a3b3f5f17f457bbf04fb8091792c20c1c8c1
Misael Flores <misaelflores4190@gmail.com>
2023-05-16 00:45:27 -0300

    Finalizacion TP Integrador Javascript, funcionamiento dinamico webApp o enlace entre links.

---
+++ T.P. Integrador Front/Script/Changes.js
@@ -0,0 +1,22 @@
+///             Dinamic WebApp Emulation           ///
+//Ocultar pagina de compras.
+window.onload = function(){
+    var buyTK = document.getElementById("BuyTK");
+    buyTK.style.display = "none";
+}
+
+//Cambio a Home
+function changeHome() {
+    var buyTK = document.getElementById("BuyTK");
+    buyTK.style.display = "none";
+    var home = document.getElementById("Home");
+    home.style.display = "block";
+}
+
+//Cambio a pagina de compras
+function changeBuyTK() {
+    var buyTK = document.getElementById("Home");
+    buyTK.style.display = "none";
+    var home = document.getElementById("BuyTK");
+    home.style.display = "block";
+}
---
+++ T.P. Integrador Front/Script/Functions.js
@@ -0,0 +1,75 @@
+///             Resume Function             /// 
+//Click alert Trigger
+const alertTrigger = document.getElementById('resumeBTN');
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert();
+  })
+}
+
+
+//Alert Append/Creation
+function appendAlert(){
+    //Creo el mensaje despues de recuperar los valores actuales del formulario.
+    var message = resumeAlert();
+
+    //Creacion de la alerta
+    var alert = document.createElement('div');
+    alert.classList.add('alert','alert-primary','alert-dismissible');
+    alert.setAttribute('role','alert');    
+
+    //Creacion icono de billete
+    var icon = document.createElement('i');
+    icon.classList.add('bi','bi-cash');
+    icon.innerHTML = message;
+
+    //Creacion boton de cerrado
+    var closeBtn = document.createElement('button');
+    closeBtn.classList.add('btn-close');
+    closeBtn.type = 'button';
+    closeBtn.setAttribute("data-bs-dismiss", "alert");
+    closeBtn.setAttribute("aria-label", "Close");
+
+    //Carga conjunta de icono y creacion dentro de la Alerta.
+    alert.appendChild(icon);
+    alert.appendChild(closeBtn);
+
+    //Recupera Div con id
+    var alertPlaceholder = document.getElementById('alertLiveResume');
+
+    //Elimina toda alerta anterior a la que se va a crear, revisa si existe alguna en el Div
+    if(alertPlaceholder.firstChild) {
+        alertPlaceholder.removeChild(alertPlaceholder.firstChild);
+    }
+
+    //Carga la alerta en el div
+    alertPlaceholder.appendChild(alert);
+
+    //Duracion de la alerta, 10 segundos
+    setTimeout(function() {
+        alertPlaceholder.removeChild(alert);
+    }, 5000);
+
+  }
+  
+//Funcion para obtener total con o sin descuento.
+ function resumeAlert(){
+    var quantity = document.getElementById("floatingQuantity").value;
+    var category = document.getElementById("floatingCategory").value;
+    var subTotal = 200 * quantity;
+    var message = " Total a Pagar: $"
+    switch(category) {
+        case "estudiante":
+            return message + (subTotal - (subTotal * 0.8));
+
+        case "trainee":
+            return  message + (subTotal - (subTotal * 0.5));
+
+        case "junior":
+            return  message + (subTotal - (subTotal * 0.15));
+
+        default:
+            return  message + subTotal;
+    }        
+}
